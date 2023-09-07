3fab080bc02b1c84995a1927d754d09a4658e67f
Matsumitsu03 <matsumitsu173@gmail.com>
2023-09-07 21:48:22 +0800

    Logout Btn Color Change

--- src/components/Header.js
+++ src/components/Header.js
@@ -19,10 +19,11 @@ const Header = () => {
         <Link to="/" className="bg-blue-400 rounded-lg p-2 m-2 font-bold text-zinc-950">Home</Link>
         <Link to="/about" className="bg-blue-400 rounded-lg p-2 m-2 font-bold text-zinc-950">About</Link>
         <Link to="/account" className="bg-blue-400 rounded-lg p-2 m-2 font-bold text-zinc-950">Account</Link>
-        <svg onClick={handleLogout} xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"
-          className='absolute inset-y-3 right-2 w-9 h-9'>
+        <button onClick={handleLogout} className='absolute inset-y-3 right-2 w-9 h-9'>
+        <svg  xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" className='w-8 h-8 fill-sky-200'>
           <path d="M16 9v-4l8 7-8 7v-4h-8v-6h8zm-16-7v20h14v-2h-12v-16h12v-2h-14z"/>
         </svg>
+      </button>
       </div>
     </div>
   );
